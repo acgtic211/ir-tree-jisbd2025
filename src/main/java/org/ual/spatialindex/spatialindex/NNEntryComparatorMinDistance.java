00c4c6c8bf24717d55a7e8d0df1c6f9084438355
Francisco José García García <frangarcj@gmail.com>
2024-09-05 21:02:57 +0200

    fix: correct order

--- src/main/java/org/ual/spatialindex/spatialindex/NNEntryComparatorMinDistance.java
+++ src/main/java/org/ual/spatialindex/spatialindex/NNEntryComparatorMinDistance.java
@@ -8,8 +8,8 @@ import java.util.Comparator;
 public class NNEntryComparatorMinDistance implements Comparator<NNEntry> {
     @Override
     public int compare(NNEntry n1, NNEntry n2) {
-        if (n1.minDistance < n2.minDistance) return 1;
-        if (n1.minDistance > n2.maxDistance) return -1;
+        if (n1.minDistance < n2.minDistance) return -1;
+        if (n1.minDistance > n2.maxDistance) return 1;
         return 0;
     }
 }
