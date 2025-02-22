195cf773630abc00766ffcd29b461192ec5130c3
Raúl García M <tukaramhl@users.noreply.github.com>
2025-02-22 22:53:13 +0100

    Add PARKS_SET dataset and update DatasetParameters to include top-k words. (NOTE: PARKS_SET files are NOT in the repo)

--- src/main/java/org/ual/spatialindex/parameters/DatasetParameters.java
+++ src/main/java/org/ual/spatialindex/parameters/DatasetParameters.java
@@ -9,8 +9,9 @@ public class DatasetParameters {
     public final double longitudeEnd;
     public final int uniqueKeywords;
     public final double maxD;
+    public final int[] topkWords; // Top-k words for each dataset. Calculated in KeywordsAnalyzer.java
 
-    public DatasetParameters(String keywordFile, String locationFile, double latitudeStart, double latitudeEnd, double longitudeStart, double longitudeEnd, int uniqueKeywords) {
+    public DatasetParameters(String keywordFile, String locationFile, double latitudeStart, double latitudeEnd, double longitudeStart, double longitudeEnd, int uniqueKeywords, int[] topkWords) {
         this.keywordFile = keywordFile;
         this.locationFile = locationFile;
         this.latitudeStart = latitudeStart;
@@ -18,6 +19,7 @@ public class DatasetParameters {
         this.longitudeStart = longitudeStart;
         this.longitudeEnd = longitudeEnd;
         this.uniqueKeywords = uniqueKeywords;
+        this.topkWords = topkWords;
         this.maxD = Math.sqrt((latitudeEnd - latitudeStart) * (latitudeEnd - latitudeStart)
                 + (longitudeEnd - longitudeStart) * (longitudeEnd - longitudeStart));
     }
--- src/main/java/org/ual/spatialindex/parameters/ParametersFactory.java
+++ src/main/java/org/ual/spatialindex/parameters/ParametersFactory.java
@@ -3,16 +3,20 @@ package org.ual.spatialindex.parameters;
 public class ParametersFactory {
     public static DatasetParameters getParameters(Dataset dataset) {
         switch (dataset) {
+            case PAPER_SET:
+                return new DatasetParameters("src/main/resources/data/paper_keywords.txt", "src/main/resources/data/paper_locations.txt", 0, 30, 0, 27, 5, new int[]{2, 1, 3, 5, 4});
             case TESTING_SET:
-                return new DatasetParameters("src/main/resources/data/key_test.txt", "src/main/resources/data/loc_test.txt", -5, 5, -4, 8, 7);
+                return new DatasetParameters("src/main/resources/data/key_test.txt", "src/main/resources/data/loc_test.txt", -5, 5, -4, 8, 7, new int[]{5, 2, 1, 3, 4, 6, 10});
             case ORIGINAL_SET:
-                return new DatasetParameters("src/main/resources/data/keywords.txt", "src/main/resources/data/locations.txt", 31, 49, -118, -81, 6);
+                return new DatasetParameters("src/main/resources/data/keywords.txt", "src/main/resources/data/locations.txt", 31, 49, -118, -81, 6, new int[]{5, 1, 2, 4, 3, 6});
             case HOTEL_SET:
-                return new DatasetParameters("src/main/resources/data/hotel_doc", "src/main/resources/data/hotel_loc", 19, 70, -159, -68, 600);
+                return new DatasetParameters("src/main/resources/data/hotel_doc", "src/main/resources/data/hotel_loc", 19, 70, -159, -68, 600, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
             case POSTAL_CODES_SET:
-                return new DatasetParameters("src/main/resources/data/postal_doc.txt", "src/main/resources/data/postal_loc.txt", -176, 180, -159, 74, 549405);
+                return new DatasetParameters("src/main/resources/data/postal_doc.txt", "src/main/resources/data/postal_loc.txt", -176, 180, -159, 74, 549405, new int[]{1, 8, 6, 872, 892, 890, 878, 882, 881, 898});
             case SPORTS_SET:
-                return new DatasetParameters("src/main/resources/data/sports_doc.txt", "src/main/resources/data/sports_loc.txt", -180, 180, -90, 79, 452950);
+                return new DatasetParameters("src/main/resources/data/sports_doc.txt", "src/main/resources/data/sports_loc.txt", -180, 180, -90, 79, 452950, new int[]{82, 138, 1, 164, 202, 118, 3462, 203, 17, 907});
+            case PARKS_SET:
+                return new DatasetParameters("src/main/resources/data/parks_doc.txt", "src/main/resources/data/parks_loc.txt", -180, 180, -90, 81, 1002722, new int[]{15, 3, 4, 384, 39, 156, 0, 1, 25, 1162});
             default:
                 throw new IllegalArgumentException("Unknown dataset: " + dataset);
         }
