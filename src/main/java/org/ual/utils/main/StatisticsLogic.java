d713ff8abbeb49fa7e06a3466b65a6bbc02f1286
Raúl García M <tukaramhl@users.noreply.github.com>
2025-02-22 22:49:24 +0100

    Refactor resultWriter in StatisticsLogic to reduce code duplication and fix buffer leak

--- src/main/java/org/ual/utils/main/StatisticsLogic.java
+++ src/main/java/org/ual/utils/main/StatisticsLogic.java
@@ -87,68 +87,97 @@ public class StatisticsLogic {
         StatisticsLogic.resultWriter(queriesStats, metricsDirectoryPath, true);
         StatisticsLogic.resultWriter(queriesStats, metricsDirectoryPath, false);
 
+        queriesStats.clear(); // Fix a "buffer leak" when changing the query type
+
         logger.info("Done");
     }
 
 
     // Write human-readable file
+//    public static void resultWriter(HashMap<String, QueryStats> queriesStats, String metricsDirectoryPath, boolean writeCSV) {
+//        for (Map.Entry<String, QueryStats> qryType : queriesStats.entrySet()) {
+//            if (!qryType.getValue().groupSizes.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "GroupSize"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().groupSizes);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().groupSizes);
+//            }
+//            if (!qryType.getValue().alphas.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "Alpha"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().alphas);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().alphas);
+//            }
+//            if (!qryType.getValue().numKeywords.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "NumberKeywords"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().numKeywords);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().numKeywords);
+//            }
+//            if (!qryType.getValue().percentages.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "Percentages"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().percentages);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().percentages);
+//            }
+//            if (!qryType.getValue().keyboardSpaceSizes.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "KeywordSpaceSize"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().keyboardSpaceSizes);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().keyboardSpaceSizes);
+//            }
+//            if (!qryType.getValue().querySpaceAreas.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "SpaceArea"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().querySpaceAreas);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().querySpaceAreas);
+//            }
+//            if (!qryType.getValue().radii.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "Radius"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().radii);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().radii);
+//            }
+//            if (!qryType.getValue().topks.isEmpty()) {
+//                String fileName = "[" + qryType.getKey() + "]" + "TopK"; // [Aggregate] alpha
+//                if (writeCSV)
+//                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().topks);
+//                else
+//                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().topks);
+//            }
+//        }
+//    }
+
     public static void resultWriter(HashMap<String, QueryStats> queriesStats, String metricsDirectoryPath, boolean writeCSV) {
         for (Map.Entry<String, QueryStats> qryType : queriesStats.entrySet()) {
-            if (!qryType.getValue().groupSizes.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "GroupSize"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().groupSizes);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().groupSizes);
-            }
-            if (!qryType.getValue().alphas.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "Alpha"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().alphas);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().alphas);
-            }
-            if (!qryType.getValue().numKeywords.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "NumberKeywords"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().numKeywords);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().numKeywords);
-            }
-            if (!qryType.getValue().percentages.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "Percentages"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().percentages);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().percentages);
-            }
-            if (!qryType.getValue().keyboardSpaceSizes.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "KeywordSpaceSize"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().keyboardSpaceSizes);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().keyboardSpaceSizes);
-            }
-            if (!qryType.getValue().querySpaceAreas.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "SpaceArea"; // [Aggregate] alpha
-                writeTXT(metricsDirectoryPath, fileName, qryType.getValue().querySpaceAreas);
-            }
-            if (!qryType.getValue().radii.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "Radius"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().radii);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().radii);
-            }
-            if (!qryType.getValue().topks.isEmpty()) {
-                String fileName = "[" + qryType.getKey() + "]" + "TopK"; // [Aggregate] alpha
-                if (writeCSV)
-                    writeCSV(metricsDirectoryPath, fileName, qryType.getValue().topks);
-                else
-                    writeTXT(metricsDirectoryPath, fileName, qryType.getValue().topks);
-            }
+            writeData(metricsDirectoryPath, qryType.getKey(), "GroupSize", qryType.getValue().groupSizes, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "Alpha", qryType.getValue().alphas, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "NumberKeywords", qryType.getValue().numKeywords, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "Percentages", qryType.getValue().percentages, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "KeywordSpaceSize", qryType.getValue().keyboardSpaceSizes, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "SpaceArea", qryType.getValue().querySpaceAreas, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "Radius", qryType.getValue().radii, writeCSV);
+            writeData(metricsDirectoryPath, qryType.getKey(), "TopK", qryType.getValue().topks, writeCSV);
         }
     }
+
+    private static void writeData(String metricsDirectoryPath, String queryKey, String dataType, List<QueryStatsData> data, boolean writeCSV) {
+        if (!data.isEmpty()) {
+            String fileName = "[" + queryKey + "]" + dataType;
+            if (writeCSV)
+                writeCSV(metricsDirectoryPath, fileName, data);
+            else
+                writeTXT(metricsDirectoryPath, fileName, data);
+        }
+    }
+
 
     private static void writeTXT(String metricsDirectoryPath, String fileName, List<QueryStatsData> qryData) {
         try (FileWriter fw = new FileWriter(metricsDirectoryPath + fileName + ".txt", true);
